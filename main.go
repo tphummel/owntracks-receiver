6c533164ed6bb6f0e4d78eda78d820143e80f2a4
Tom Hummel <tohu@hey.com>
2023-05-08 23:27:31 -0700

    add all owntracks fields

--- main.go
+++ main.go
@@ -11,11 +11,33 @@ import (
 )
 
 type LocationUpdate struct {
-	Type      string  `json:"_type"`
-	Longitude float64 `json:"lon"`
-	Latitude  float64 `json:"lat"`
-	Timestamp int64   `json:"tst"`
+	Type       string    `json:"_type"`
+	Acc        *int      `json:"acc,omitempty"`
+	Alt        *int      `json:"alt,omitempty"`
+	Batt       *int      `json:"batt,omitempty"`
+	BS         *int      `json:"bs,omitempty"`
+	Cog        *int      `json:"cog,omitempty"`
+	Latitude   float64   `json:"lat"`
+	Longitude  float64   `json:"lon"`
+	Rad        *int      `json:"rad,omitempty"`
+	T          string    `json:"t,omitempty"`
+	TID        string    `json:"tid,omitempty"`
+	Timestamp  int64     `json:"tst"`
+	Vac        *int      `json:"vac,omitempty"`
+	Vel        *int      `json:"vel,omitempty"`
+	P          *float64  `json:"p,omitempty"`
+	POI        *string   `json:"poi,omitempty"`
+	Conn       *string   `json:"conn,omitempty"`
+	Tag        *string   `json:"tag,omitempty"`
+	Topic      string    `json:"topic"`
+	InRegions  *[]string `json:"inregions,omitempty"`
+	InRIDs     *[]string `json:"inrids,omitempty"`
+	SSID       *string   `json:"SSID,omitempty"`
+	BSSID      *string   `json:"BSSID,omitempty"`
+	CreatedAt  *string   `json:"created_at,omitempty"`
+	Monitoring *int      `json:"m,omitempty"`
 }
+
 
 func initDB(filename string) *sql.DB {
 	db, err := sql.Open("sqlite3", filename)
@@ -39,15 +61,44 @@ func initDB(filename string) *sql.DB {
 }
 
 func saveLocationUpdate(db *sql.DB, locationUpdate *LocationUpdate) error {
-	stmt, err := db.Prepare("INSERT INTO location_updates (type, longitude, latitude, timestamp) VALUES (?, ?, ?, ?)")
+	stmt, err := db.Prepare(`INSERT INTO location_updates (
+		type, acc, alt, batt, bs, cog, latitude, longitude, rad, t, tid, timestamp, vac, vel, p, poi, conn, tag, topic, inregions, inrids, ssid, bssid, created_at, monitoring
+	) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(locationUpdate.Type, locationUpdate.Longitude, locationUpdate.Latitude, locationUpdate.Timestamp)
+	_, err = stmt.Exec(
+		locationUpdate.Type,
+		locationUpdate.Acc,
+		locationUpdate.Alt,
+		locationUpdate.Batt,
+		locationUpdate.BS,
+		locationUpdate.Cog,
+		locationUpdate.Latitude,
+		locationUpdate.Longitude,
+		locationUpdate.Rad,
+		locationUpdate.T,
+		locationUpdate.TID,
+		locationUpdate.Timestamp,
+		locationUpdate.Vac,
+		locationUpdate.Vel,
+		locationUpdate.P,
+		locationUpdate.POI,
+		locationUpdate.Conn,
+		locationUpdate.Tag,
+		locationUpdate.Topic,
+		locationUpdate.InRegions,
+		locationUpdate.InRIDs,
+		locationUpdate.SSID,
+		locationUpdate.BSSID,
+		locationUpdate.CreatedAt,
+		locationUpdate.Monitoring,
+	)
 	return err
 }
+
 
 func handleLocationUpdate(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
